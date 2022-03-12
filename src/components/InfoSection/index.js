b3cfa9e5e3bb8ff8e5a39e7e0d185d621045c7fb
azmir849 <azmir15-849@diu.edu.bd>
2022-03-12 10:26:40 +0600

    apply smooth scroll to button

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -30,7 +30,7 @@ const InfoSection = ({
   alt,
   primary,
   dark,
-  dark2
+  dark2,
 }) => {
   return (
     <>
@@ -43,16 +43,19 @@ const InfoSection = ({
                 <Heading lightText={lightText}>{headline}</Heading>
                 <Subtitle darkText={darkText}>{description}</Subtitle>
                 <BtnWrap>
-                  <Button to='home'
-                  smooth={true}
-                  duration={500}
-                  spy={true}
-                  exact='true'
-                  offset={-80}
-                  primary={primary ? 1 : 0}
-                  dark={dark ? 1 : 0}
-                  dark2={dark2 ? 1 : 0}
-                  >{buttonLabel}</Button>
+                  <Button
+                    to='home'
+                    smooth={true}
+                    duration={2000}
+                    spy={true}
+                    exact='true'
+                    offset={-80}
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                  >
+                    {buttonLabel}
+                  </Button>
                 </BtnWrap>
               </TextWrapper>
             </Column1>
